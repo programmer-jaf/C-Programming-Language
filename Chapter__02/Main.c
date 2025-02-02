a9672526f4401c4d0f98d5b9404362be0781ffd6
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-02 18:21:16 +0600

    Fix typo in Main.c comments and add new variables.c example

--- Chapter__02/Main.c
+++ Chapter__02/Main.c
@@ -6,7 +6,7 @@ int main() {
   * #include <stdio.h> --> Preprossor dirctive
   * in every Programming language just like C/C++/Java program file the entry point of the program is Main function/method
   * When the program start then the Main function will be called
-  * printf("Break down the main program"); it menas that when main function will be called the line ["Break down the main program"] will be printed as an output line
+  * printf("Break down the main program"); it means that when main function will be called the line ["Break down the main program"] will be printed as an output line
   * return 0 it means the program will end successfully
   */
   return 0;
---
+++ Chapter__03/variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  printf("variables in c Programming language\n");
+  printf("-------------------------------- Variables --------------------------------","\n");
+  printf("Variables are container to store values. For example, If you want to store Baby Milk you have to store it in a milk bottle \n");
+  int num = 124;
+
+  printf("num = %d", num);
+  return 0;
+}
