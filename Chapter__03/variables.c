c84180e4c49a57ce2cbbf15151de2990911b3183
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-02 18:24:54 +0600

    Add variable naming rules as comments in variables.c

--- Chapter__03/variables.c
+++ Chapter__03/variables.c
@@ -7,5 +7,13 @@ int main(){
   int num = 124;
 
   printf("num = %d", num);
+  /*
+  ! Rules while creating variables
+  1. A variable name must start with a letter or underscore
+  2. A variable name cannot start with a number
+  3. A variable name can only contain alpha-numeric characters and underscores
+  4. Variable names are case-sensitive (myVariable and myvariable are different variables)
+  5. Variable names cannot be a reserved keyword in C (e.g., int, float, double, char, etc.)
+  */
   return 0;
 }
