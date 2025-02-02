7f8ab88622bcff3f9716e5472ed7b886efab5aa0
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-02 17:51:38 +0600

    Break down the C Program with Comments

---
+++ Chapter__02/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("Break down the Main program");
+  /*
+  * #include <stdio.h> --> Preprossor dirctive
+  * in every Programming language just like C/C++/Java program file the entry point of the program is Main function/method
+  * When the program start then the Main function will be called
+  * printf("Break down the main program"); it menas that when main function will be called the line ["Break down the main program"] will be printed as an output line
+  * return 0 it means the program will end successfully
+  */
+  return 0;
+}
