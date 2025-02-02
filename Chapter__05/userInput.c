32c6ed3a0da3850c90def2aba0f38340e5db474c
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-02 18:56:15 +0600

    Learn How to take input from the user

---
+++ Chapter__05/userInput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  int num = 0;
+  printf("Taking input from the user\n");
+  // * scanf is used to take input from the user by using keyboard
+  scanf("%d", &num);
+  printf("You entered: %d\n", num);
+  return 0;
+}
